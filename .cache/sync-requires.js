9c211ed82d0d579a1e8eb2a8213a238215a0ee2a
pung8146 <pung8146@naver.com>
2021-11-10 10:37:36 +0900

    Oh..

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Hoon\\blogGatsby\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-tsx": preferDefault(require("C:\\Users\\Hoon\\blogGatsby\\src\\pages\\404.tsx")),
+  "component---src-pages-index-tsx": preferDefault(require("C:\\Users\\Hoon\\blogGatsby\\src\\pages\\index.tsx")),
+  "component---src-pages-info-tsx": preferDefault(require("C:\\Users\\Hoon\\blogGatsby\\src\\pages\\info.tsx"))
+}
+
